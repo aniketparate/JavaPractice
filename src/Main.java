9a4c64586c35650bb56077f427b3dd59118c36eb
aniketparate <aniketvparate@gmail.com>
2021-09-18 13:42:12 +0530

    add

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ public class Main {
     public static void main(String[] args) {
         // var DataType can be used for any type of data
         var a = 512_343;
+        var c = "Hello";
+        var d = 'W';
         int b = 3_431_231;
 
         System.out.println(a + b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
