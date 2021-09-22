edfd8802e9ac5e470df227b730be055f39d05914
aniketparate <aniketvparate@gmail.com>
2021-09-22 23:58:51 +0530

    add

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // var DataType can be used for any type of data
-        var a = 512_343;
-        var c = "Hello";
-        var d = 'W';
-        int b = 3_431_231;
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println(a + b);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = 3_431_231;
+
+        boolean value = ((a < b || a > c) && (a < c || a > b)) || (a == b);
+        System.out.println(value);
         System.out.println(c);
         System.out.println(d);
     }
--- src/relationalOperators.java
+++ src/relationalOperators.java
@@ -1,11 +1,30 @@
 import java.util.Scanner;
-
+//Write a program that reads the numbers a, b, c and checks if there's a pair of them that adds up to exactly 20.
+//
+//The program must output true or false.
 public class relationalOperators {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        boolean value = (a + b) == 20 || (a + c) == 20 || (b + c) == 20;
+
+        System.out.println(value);
     }
 }
+
+/*
+Write a program that reads three integer numbers and prints true if the first number
+is between the second and the third one (inclusive). Otherwise, it must print false.
+The last two arguments may not be sorted.
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        boolean value = a < b && a > c || a < c && a > b || a == b;
+        System.out.println(value);
+ */
