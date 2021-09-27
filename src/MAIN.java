9c518f356fc06212af194210a2defdb6f4edf96d
aniketparate <aniketvparate@gmail.com>
2021-09-27 14:09:16 +0530

    add

---
+++ src/MAIN.java
@@ -0,0 +1,18 @@
+public class MAIN {
+    public static void main(String[] args) {
+
+        int a = 1;
+
+        for(;a <= 100;a++){
+            if(a % 3 == 0 && a % 5 == 0){
+                System.out.println("HELLO WORLD");
+            } else if (a % 5 == 0){
+                System.out.println("WORLD");
+            } else if (a % 3 == 0){
+                System.out.println("HELLO");
+            } else {
+                System.out.println(+a);
+            }
+        }
+    }
+}
