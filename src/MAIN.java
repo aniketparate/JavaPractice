287d971952f47c15596f0d9dd2483b5a1e14abbf
aniketparate <aniketvparate@gmail.com>
2021-10-01 12:15:02 +0530

    add

--- src/MAIN.java
+++ src/MAIN.java
@@ -1,18 +1,56 @@
+import java.util.Scanner;
+
+class employee {
+    double Employee_no;
+    String Empolyee_name;
+    double basic;
+    double DA;
+    double ID;
+    double IT;
+    double netSalary;
+
+    void employeeDetails (double employee_no, String name, double ID, double basic) {
+        this.Employee_no = employee_no;
+        this.Empolyee_name = name;
+        this.ID = ID;
+        this.basic = basic;
+    }
+
+    void netSalary(double basic){
+        this.DA = 1.32 * basic;
+        this.IT = 0.3 * (basic + this.DA);
+        this.netSalary = (basic + this.DA) - this.IT;
+    }
+
+    void display() {
+        System.out.println(" Employee No : " + Employee_no);
+        System.out.println(" Employee Name : " + Empolyee_name);
+        System.out.println(" ID : " + ID);
+        System.out.println(" Basic : " + basic);
+        System.out.println(" DA : " + DA);
+        System.out.println(" IT : " + IT);
+        System.out.println(" Net Salary : " + netSalary);
+    }
+}
+
 public class MAIN {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int a = 1;
+        employee employ = new employee();
 
-        for(;a <= 100;a++){
-            if(a % 3 == 0 && a % 5 == 0){
-                System.out.println("HELLO WORLD");
-            } else if (a % 5 == 0){
-                System.out.println("WORLD");
-            } else if (a % 3 == 0){
-                System.out.println("HELLO");
-            } else {
-                System.out.println(+a);
-            }
-        }
+        System.out.print(" Enter Employee No : ");
+        double no = sc.nextInt();
+        System.out.print(" Enter Employee Name : ");
+        String name = sc.next();
+        System.out.print("  Enter Employee ID : ");
+        double ID = sc.nextInt();
+        System.out.print(" Enter basic : ");
+        double basic = sc.nextInt();
+
+        employ.employeeDetails(no, name, ID, basic);
+        employ.netSalary(basic);
+        employ.display();
+
     }
 }
