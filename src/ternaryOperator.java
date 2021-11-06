8e8ecafbfbac8dc5bf2bd920eb374099eeb838d5
aniketparate <aniketvparate@gmail.com>
2021-11-06 10:24:11 +0530

    docs: added ternaryOperator

---
+++ src/ternaryOperator.java
@@ -0,0 +1,8 @@
+
+
+public class ternaryOperator {
+    public static void main(String[] args) {
+        // Syntax to declare a condition for true or false
+        // result = condition ? trueCase : elseCase;
+    }
+}
